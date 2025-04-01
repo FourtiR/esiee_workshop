fe22015cc5cc99322d4a897a5b06d76de2f1a1ce
FourtiR <113357324+FourtiR@users.noreply.github.com>
2025-04-01 19:49:37 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+x = 150
+y = “Marceille"
+
+z = x+y
+
+name = "Rayan"
+surname = "9206"
+
+student_name = name + " " +surname
+
+a = 10
+b = 5
+c = a - b
+d = a * b
+e = a % b
+print(x)
+print(y)
+print(stundent_name)
+Hello World 20.0
